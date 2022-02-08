0b5e0dcb91902cf4256b9d4e05d0a4e5e04533a3
foqiashahid112 <foqiashahid112@gmail.com>
2022-02-08 23:29:21 +0500

    assignment 2 complete

--- A02/dynamic_snackbar.c
+++ A02/dynamic_snackbar.c
@@ -9,5 +9,24 @@ struct snack {
 };
 
 int main() {
+  int n;
+  printf("Enter a number of snacks: ");
+  scanf("%d",&n);
+  //struct snack* allSnacks = malloc(sizeOf(struct snack)*n); //pointer to array
+  struct snack allSnacks[n];
+  for(int i = 0; i < n; i++){
+    printf("Enter a name: ");
+    scanf("%s", allSnacks[i].name);
+    printf("Enter a cost: ");
+    scanf("%f", &allSnacks[i].cost);
+    printf("Enter a quantity: ");
+    scanf("%d", &allSnacks[i].quantity);
+  }
+
+  printf("Welcome to Dynamic Donna's Snack Bar. \n");
+  printf("\n");
+  for(int i = 0; i < n; i++){
+    printf("%d) %s      cost: $%f   quantity: %d \n", i, allSnacks[i].name, allSnacks[i].cost, allSnacks[i].quantity);
+  } 
   return 0;
 }
--- A02/repeat.c
+++ A02/repeat.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
+  char s[100];
+  int n;
+  printf("Enter a word: ");
+  scanf("%s",s);
+  printf("Enter a count: ");
+  scanf("%d",&n);
+  
+  int sizeIString = strlen(s);
+  int sizeOString = sizeIString * n;
+  char* output = (char*)malloc(sizeof(char)*sizeOString + 1);
+  if(output == NULL){
+    printf("Cannot allocate new string. Exciting...\n");
+    exit(0);
+  }
+  for(int i = 0; i < n; i++){
+    strcat(output, s);
+  }
+  printf("Your word is %s\n", output);
+  free(output);
   return 0;
 }
