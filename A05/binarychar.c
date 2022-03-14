75bce2666a0d1d646a5d350f5017af1246ead3f6
foqiashahid112 <foqiashahid112@gmail.com>
2022-03-13 23:57:40 -0400

    changes

--- A05/binarychar.c
+++ A05/binarychar.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+
+
+int binaryToDecimal(char* inputBinary){
+  int decimal = 0;
+  int multiple = 1; //pow(2,0)
+  for(int i = 7; i >= 0; i--){
+    if(inputBinary[i] == '1') decimal += multiple;
+    multiple = multiple * 2;
+  }
+  return decimal;
+}
+
+
 int main() {
+  char input[8];
+  printf("Enter 8 bits: ");
+  scanf("%s", input);
+  int decimal = binaryToDecimal(input);
+  printf("binary to decimal %d", decimal);
+  //printf("Your character is: %c\n", character);
   return 0;
 }
