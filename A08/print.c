0b2007d996069e8f6400f4eb5731e7e168a9369b
Foqia Shahid <foqiashahid112@gmail.com>
2022-04-04 03:08:50 -0400

    added print.c

--- A08/print.c
+++ A08/print.c
@@ -5,6 +5,15 @@
 #include "rand.h"
 
 int main (int argc, char* argv[]) {
+  void *init = sbrk(0);
+  printf("The intial top of the heap is %p. \n", init);
+  
+  void *current;
+  current = sbrk(0);
 
+  printf("The current top of the heap is %p. \n", current);
+  int allocated = (int) (current - init);
+  printf("Increased by %d (0x%x) bytes \n", allocated, allocated);
+  
   return 0 ;
 }
